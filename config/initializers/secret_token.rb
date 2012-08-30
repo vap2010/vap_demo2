3644e244d146b1bea8ebdf5a418a5e9bb6c9491e
Vadim Panchenko <vadim-ip@mail.ru>
2012-08-31 01:45:56 +0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+VapDemo::Application.config.secret_token = 'f3f98d90407fb575d73b868a2bfb433001ff77be430b1c73a86ffab18f77d9450029501973941810823682c8ab703667e44ef0ce36e378db409a6f2bbfc72399'
